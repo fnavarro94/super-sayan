61a6dd7e2f737ae381ced33455000e02ca608bb2
Felipe Navarro <felinavarro94@gmail.com>
2019-05-20 18:01:57 -0500

    saving

--- super_saiyan/helpers/JetReCalibrator_saiyan.py
+++ super_saiyan/helpers/JetReCalibrator_saiyan.py
@@ -1,18 +1,16 @@
 import numpy as np
 import awkward
+import sys
 import uproot, uproot_methods
+sys.path.append("/home/felipe/saiyan/saiyan/saiyan/arrays")
 import Builder  # (saiyan tools)
-import JetCorrectionParser
+from JetCorrectionParser import CorrectionParser
+
+
 
 
 class JetRecalibrator_saiyan:
-	def __init__(self,globalTag,jetFlavour,doResidualJECs,jecPath,upToLevel=3,
-                 calculateSeparateCorrections=False,
-                 calculateType1METCorrection=False, type1METParams={'jetPtThreshold':15., 'skipEMfractionThreshold':0.9, 'skipMuons':True} ):
-        """Create a corrector object that reads the payloads from the text dumps of a global tag under
-            CMGTools/RootTools/data/jec  (see the getJec.py there to make the dumps).
-           It will apply the L1,L2,L3 and possibly the residual corrections to the jets.
-           If configured to do so, it will also compute the type1 MET corrections."""
+    def __init__(self,globalTag,jetFlavour,doResidualJECs,jecPath,upToLevel=3,calculateSeparateCorrections=False,calculateType1METCorrection=False, type1METParams={'jetPtThreshold':15., 'skipEMfractionThreshold':0.9, 'skipMuons':True} ):
         self.globalTag = globalTag
         self.jetFlavour = jetFlavour
         self.doResidualJECs = doResidualJECs
@@ -22,7 +20,7 @@ class JetRecalibrator_saiyan:
         self.type1METParams  = type1METParams
         # Make base corrections
         #path = os.path.expandvars(jecPath) #"%s/src/CMGTools/RootTools/data/jec" % os.environ['CMSSW_BASE'];
-        
+        path = self.jecPath
         self.L1JetPar  = CorrectionParser("%s/%s_L1FastJet_%s.txt" % (path,globalTag,jetFlavour),"");
         self.L2JetPar  = CorrectionParser("%s/%s_L2Relative_%s.txt" % (path,globalTag,jetFlavour),"");
         self.L3JetPar  = CorrectionParser("%s/%s_L3Absolute_%s.txt" % (path,globalTag,jetFlavour),"");
@@ -35,7 +33,7 @@ class JetRecalibrator_saiyan:
             self.ResJetPar = CorrectionParser("%s/%s_L2L3Residual_%s.txt" % (path,globalTag,jetFlavour))
             self.vPar.append(self.ResJetPar);
         #Step3 (Construct a FactorizedJetCorrector object) 
-        self.JetCorrector = ROOT.FactorizedJetCorrector(self.vPar)
+        #self.JetCorrector = ROOT.FactorizedJetCorrector(self.vPar)
         if os.path.exists("%s/%s_Uncertainty_%s.txt" % (path,globalTag,jetFlavour)):
             self.JetUncertainty = UncertaintyParser("%s/%s_Uncertainty_%s.txt" % (path,globalTag,jetFlavour));
         elif os.path.exists("%s/Uncertainty_FAKE.txt" % path):
@@ -44,19 +42,19 @@ class JetRecalibrator_saiyan:
             print 'Missing JEC uncertainty file "%s/%s_Uncertainty_%s.txt", so jet energy uncertainties will not be available' % (path,globalTag,jetFlavour)
             self.JetUncertainty = None
         self.separateJetCorrectors = {}
-        if calculateSeparateCorrections or calculateType1METCorrection:
-            self.vParL1 = ROOT.vector(ROOT.JetCorrectorParameters)()
-            self.vParL1.push_back(self.L1JetPar)
-            self.separateJetCorrectors["L1"] = ROOT.FactorizedJetCorrector(self.vParL1)
-            if upToLevel >= 2 and calculateSeparateCorrections:
-                self.vParL2 = ROOT.vector(ROOT.JetCorrectorParameters)()
-                for i in [self.L1JetPar,self.L2JetPar]: self.vParL2.push_back(i)
-                self.separateJetCorrectors["L1L2"] = ROOT.FactorizedJetCorrector(self.vParL2)
-            if upToLevel >= 3 and calculateSeparateCorrections:
-                self.vParL3 = ROOT.vector(ROOT.JetCorrectorParameters)()
-                for i in [self.L1JetPar,self.L2JetPar,self.L3JetPar]: self.vParL3.push_back(i)
-                self.separateJetCorrectors["L1L2L3"] = ROOT.FactorizedJetCorrector(self.vParL3)
-            if doResidualJECs and calculateSeparateCorrections:
-                self.vParL3Res = ROOT.vector(ROOT.JetCorrectorParameters)()
-                for i in [self.L1JetPar,self.L2JetPar,self.L3JetPar,self.ResJetPar]: self.vParL3Res.push_back(i)
-                self.separateJetCorrectors["L1L2L3Res"] = ROOT.FactorizedJetCorrector(self.vParL3Res)
+        #if calculateSeparateCorrections or calculateType1METCorrection:
+        #    self.vParL1 = ROOT.vector(ROOT.JetCorrectorParameters)()
+        #    self.vParL1.push_back(self.L1JetPar)
+        #    self.separateJetCorrectors["L1"] = ROOT.FactorizedJetCorrector(self.vParL1)
+        #    if upToLevel >= 2 and calculateSeparateCorrections:
+        #        self.vParL2 = ROOT.vector(ROOT.JetCorrectorParameters)()
+        #        for i in [self.L1JetPar,self.L2JetPar]: self.vParL2.push_back(i)
+        #        self.separateJetCorrectors["L1L2"] = ROOT.FactorizedJetCorrector(self.vParL2)
+        #    if upToLevel >= 3 and calculateSeparateCorrections:
+        #        self.vParL3 = ROOT.vector(ROOT.JetCorrectorParameters)()
+        #        for i in [self.L1JetPar,self.L2JetPar,self.L3JetPar]: self.vParL3.push_back(i)
+        #        self.separateJetCorrectors["L1L2L3"] = ROOT.FactorizedJetCorrector(self.vParL3)
+        #    if doResidualJECs and calculateSeparateCorrections:
+        #        self.vParL3Res = ROOT.vector(ROOT.JetCorrectorParameters)()
+        #        for i in [self.L1JetPar,self.L2JetPar,self.L3JetPar,self.ResJetPar]: self.vParL3Res.push_back(i)
+        #        self.separateJetCorrectors["L1L2L3Res"] = ROOT.FactorizedJetCorrector(self.vParL3Res)
--- super_saiyan/helpers/sayanTest.py
+++ super_saiyan/helpers/sayanTest.py
@@ -1,10 +1,15 @@
 
 #must use python2
-
+import sys
 import uproot, uproot_methods
 import numpy as np
+sys.path.append('/home/felipe/super_sayan/super-sayan/super_saiyan/helpers')
+import JetReCalibrator_saiyan
+from JetReCalibrator_saiyan import *
 from awkward import JaggedArray, Table
-   
+
+
+JRC = JetRecalibrator_saiyan("Jec10V","AK5PF",True,"/home/felipe/super_sayan/super-sayan/super_saiyan/helpers/data")
 
   
 
@@ -15,7 +20,7 @@ columns = ['Electron_pt']
 for arrays in uproot.tree.iterate('root_files/nano.root','Events',columns,entrysteps=5000):
      
     e = arrays['Electron_pt']
-    print arrays['Electron_pt']	
+    print arrays['Electron_pt'][100:103]
 	
 
  
